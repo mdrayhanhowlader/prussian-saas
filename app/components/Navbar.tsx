aa31bc23ef1a2f2e37fca676c61c41dd749b37be
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-02-05 23:21:06 +0800

    login log out and sign up button has been handled

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { ThemeToggle } from "./Themetoggle";
 import { Button } from "@/components/ui/button";
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar(){
+export async function Navbar(){
+    const {isAuthenticated} = getKindeServerSession();
     return (
         <nav className="border-b bg-background h-[10vh] flex items-center">
             <div className="container flex items-center justify-between">
@@ -12,10 +14,15 @@ export function Navbar(){
                 </Link>
                 <div className="flex items-center gap-x-5">
                     <ThemeToggle />
+                    
+                {await isAuthenticated() ? (
+<LogoutLink><Button>Log out</Button></LogoutLink>
+                ) : (
                     <div className="flex items-center gap-x-5">
                         <LoginLink><Button>Sign In</Button></LoginLink>
                         <RegisterLink><Button variant={"secondary"}>Sign Up</Button></RegisterLink>
                     </div>
+                )}
                 </div>
             </div>
         </nav>
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage(){
+    return (
+        <div>
+            <h1>Hello from the Dashboard</h1>
+        </div>
+    )
+}
