0b8058b87144f6b87d68d48ee0fdc6408982554c
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-02-09 00:12:23 +0800

    user info has been updated dynamically

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,7 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { CreditCard, Home, Settings } from "lucide-react";
+import { CreditCard, DoorClosed, Home, Settings } from "lucide-react";
+import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
 
 import Link from "next/link";
 
@@ -12,13 +14,13 @@ export const navItems = [
     {name: 'Billing', href: '/dashboard/billing', icon: CreditCard}
 ]
 
-export function UserNav(){
+export function UserNav({name, email, image} : {name: string, email: string, image: string}){
     return (
           <DropdownMenu>
              <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="relative h-10 w-10 rounded-full">
                     <Avatar className="h-10 w-10 rounded-full">
-                        <AvatarImage src="https://github.com/shadcn.png"  alt="" />
+                        <AvatarImage src={image}  alt="" />
                         <AvatarFallback>Amar</AvatarFallback>
                     </Avatar>
                 </Button>
@@ -26,9 +28,9 @@ export function UserNav(){
             <DropdownMenuContent className="w-56" align="end" forceMount>
                 <DropdownMenuLabel>
                     <div className="flex flex-col space-y-1">
-                        <p className="text-sm font-medium leading-none">Amrul Huda</p>
+                        <p className="text-sm font-medium leading-none">{name}</p>
                         <p className="text-xs leading-none text-muted-foreground
-                        ">amar@next.tsx</p>
+                        ">{email}</p>
                     </div>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
@@ -42,6 +44,10 @@ export function UserNav(){
                         </DropdownMenuItem>
                     ))}
                 </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="w-full flex justify-between items-center" asChild>
+                    <LogoutLink>Logout <span><DoorClosed className="w-4 h-4" /></span></LogoutLink>
+                </DropdownMenuItem>
             </DropdownMenuContent> 
         </DropdownMenu>
     )
