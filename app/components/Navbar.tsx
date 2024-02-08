883b5bea74d75dd00db27a57c885866d148acf5e
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-02-08 09:18:12 +0800

    dropdown menu error has been solved

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,7 +10,7 @@ export async function Navbar(){
     return (
         <nav className="border-b bg-background h-[10vh] flex items-center">
             <div className="container flex items-center justify-between">
-                <Link href={'/'}>
+                <Link href="/">
                     <h1 className="font-bold text-3xl">Prussian<span className="text-primary">Saas</span></h1>
                 </Link>
                 <div className="flex items-center gap-x-5">
--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,11 +1,12 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
 
 export function UserNav(){
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
+          <DropdownMenu>
+             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="relative h-10 w-10 rounded-full">
                     <Avatar className="h-10 w-10 rounded-full">
                         <AvatarImage src="https://github.com/shadcn.png"  alt="" />
@@ -17,10 +18,11 @@ export function UserNav(){
                 <DropdownMenuLabel>
                     <div className="flex flex-col space-y-1">
                         <p className="text-sm font-medium leading-none">Amrul Huda</p>
-                        <p className="text-xs leading-none text-muted-foreground">amar@next.tsx</p>
+                        <p className="text-xs leading-none text-muted-foreground
+                        ">amar@next.tsx</p>
                     </div>
                 </DropdownMenuLabel>
-            </DropdownMenuContent>
+            </DropdownMenuContent> 
         </DropdownMenu>
     )
 }
