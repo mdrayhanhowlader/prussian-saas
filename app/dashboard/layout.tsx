2976d89249eb832b014339ab80c35c2603dc6950
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-02-19 23:20:38 +0800

    prisma database has been connected

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,30 @@ import { ReactNode } from "react";
 import { DashboardNav } from "../components/DashboardNav";
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import prisma from "../lib/db";
 
+async function getData({email, id, firstName, lastName, profileImage} : {email: string; id: string; firstName: string | undefined | null; lastName: string | undefined | null; profileImage: string | undefined | null;}){
+    const user = await prisma.user.findUnique({
+        where: {
+            id: id,
+        },
+        select: {
+            id: true, 
+            stripeCustomerId: true,
+        }
+    });
+    if(!user){
+        const name = `${firstName ?? ''} ${lastName ?? ''}`
+        await prisma.user.create({
+            data: {
+                id: id,
+                email: email,
+                name: name,
+                
+            }
+        })
+    }
+}
 
 export default async function DashboardLayout({children}:{children: ReactNode}){
     const {getUser} = getKindeServerSession();
@@ -10,6 +33,7 @@ export default async function DashboardLayout({children}:{children: ReactNode}){
     if(!user){
         return redirect('/');
     }
+    await getData({email: user.email as string, firstName: user.given_name as string, id: user.id as string, lastName: user.family_name as string, profileImage: user.picture})
     return (
        <div className="flex flex-col space-y-6 mt-10">
             <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
