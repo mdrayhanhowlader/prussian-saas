e752afe7f0dbe56aa4c96ff8e028e229e62d17c6
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-02-24 14:34:30 +0800

    stripe payment method has been updated successfully

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { DashboardNav } from "../components/DashboardNav";
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import prisma from "../lib/db";
+import { stripe } from "../lib/stripe";
 
 async function getData({email, id, firstName, lastName, profileImage} : {email: string; id: string; firstName: string | undefined | null; lastName: string | undefined | null; profileImage: string | undefined | null;}){
     const user = await prisma.user.findUnique({
@@ -21,10 +22,21 @@ async function getData({email, id, firstName, lastName, profileImage} : {email: 
                 id: id,
                 email: email,
                 name: name,
-            }
-        })
-    }
-}
+            },
+        });
+    };
+    if(!user?.stripeCustomerId) {
+        const data = await stripe.customers.create({email: email,});
+        await prisma.user.update({
+            where: {
+                id: id,
+            },
+            data: {
+                stripeCustomerId: data.id,
+            },
+        });
+    };
+};
 
 export default async function DashboardLayout({children}:{children: ReactNode}){
     const {getUser} = getKindeServerSession();
